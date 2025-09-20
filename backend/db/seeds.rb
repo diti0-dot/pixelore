254f7fbcd5532100e57d4dc3ba2cab562d7c6f06
dn24002 <dn24002@edu.lu.lv>
2025-09-20 15:41:13 +0000

    Ready for deployment

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,13 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-cleo = User.create!(
-  email: "cleo@gmail.com",
-  username: "cleo",
-  password: "cleo1234"
+#   
+# ===== USERS =====
+User.create!(id: 1, email: "cleouser@example.com", username: "Cleo", password: "password")
+User.create!(id: 2, email: "guest@example.com", username: "Guest", password: "password")
+User.create!(id: 34, email: "diti@example.com", username: "diti", password: "password")
+User.create!(id: 35, email: "mac@example.com", username: "mac", password: "password")
+User.create!(id: 36, email: "mac1@example.com", username: "mac1", password: "password")
+User.create!(id: 37, email: "hehehhe@example.com", username: "hehehhe", password: "password")
+User.create!(id: 38, email: "fuc111111ee@example.com", username: "fuc111111ee", password: "password")
+User.create!(id: 39, email: "iiwwa@example.com", username: "iiwwa", password: "password")
+
+# ===== POSTS =====
+post1 = Post.create!(title: "Controller", body: "controller hehhe", user_id: 2)
+post1.post_pic.attach(
+  io: File.open(Rails.root.join("db/seeds/images/controller.png")),
+  filename: "controller.png",
+  content_type: "image/png"
 )
 
-cleo.posts.create!(
-  title: "Random Thoughts",
-  body: "Just writing my first post here. Nothing fancy, just testing!"
+post2 = Post.create!(title: "Heart", body: "I wanted to draw this", user_id: 2)
+post2.post_pic.attach(
+  io: File.open(Rails.root.join("db/seeds/images/heart.png")),
+  filename: "heart.png",
+  content_type: "image/png"
 )
+
+post3 = Post.create!(title: "Pumpkin", body: "It's fall finalllyyy", user_id: 1)
+post3.post_pic.attach(
+  io: File.open(Rails.root.join("db/seeds/images/pumpkin.png")),
+  filename: "pumpkin.png",
+  content_type: "image/png"
+)
+
+
